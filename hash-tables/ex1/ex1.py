4baa0a204084c1256598dc345bf6baa05a8b3660
Marco A. Guzman <marcoguzman16@gmail.com>
2018-10-12 10:41:00 -0700

    ex1 in hash-tables

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,26 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  #initialize dictionary (hash table) to store values
+  dict = {}
+  #setting hash table (weights = keys. values = i)
+  #using enumerate to keep track of iterations w key value pairs
+  for i, weight in enumerate(weights):
+        dict[weight] = i
+  #iterating through table checking weights that meet limit
+  for i, weight in enumerate(weights):
+        if limit - weight in dict:
+              j = dict[limit - weight]
+              if i > j:
+                  return (i, j)
+              else:
+                    return (j, i)
+
+  #returning empty tuple for values not meeting limit
+  return ()
+                    
+
+
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  print(get_indices_of_item_weights([4,6,10,15,16], 21))
