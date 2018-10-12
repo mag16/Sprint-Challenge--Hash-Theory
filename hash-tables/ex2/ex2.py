a5b506852e29435e8d16b325d9670afae64ceb51
Marco A. Guzman <marcoguzman16@gmail.com>
2018-10-12 11:28:10 -0700

    ex2 completed

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,44 @@
 def reconstruct_trip(tickets):
-  pass 
+  #start hash table
+  dict = {}
+  # initialize list 
+  trips_end = []
+
+  for ticket in tickets:
+        dict[ticket[0]] = ticket[1]
+  
+  current_tick = dict[None]
+  #looping until destination is none to return list(array)
+  while current_tick != None:
+        trips_end.append(current_tick)
+        if current_tick in dict:
+              current_tick = dict[current_tick]
+              
+  return trips_end
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass
+  print(reconstruct_trip([
+      ('PIT', 'ORD'),
+      ('XNA', 'CID'),
+      ('SFO', 'BHM'),
+      ('FLG', 'XNA'),
+      (None, 'LAX'),
+      ('LAX', 'SFO'),
+      ('CID', 'SLC'),
+      ('ORD', None),
+      ('SLC', 'PIT'),
+      ('BHM', 'FLG'),
+  ]))
